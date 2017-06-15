376abd8b469bf2b8fa1a35ca60a0deb0c346b03e
zhaoyu <zhaoyuwy@163.com>
2017-06-16 00:14:11 +0800

    邮箱测试中。

---
+++ sitetool/src/main/java/com/huawei/siteapp/common/util/SendMailUtil.java
@@ -0,0 +1,96 @@
+package com.huawei.siteapp.common.util;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+import java.util.Properties;
+
+/**
+ * Created by z00390414 on 2017/6/16.
+ *
+ * @version 1.0
+ * @date 2017/6/16
+ */
+public class SendMailUtil {
+    // 设置服务器
+    private static String KEY_SMTP = "mail.smtp.host";
+    private static String VALUE_SMTP = "smtp.qq.com";
+    // 服务器验证
+    private static String KEY_PROPS = "mail.smtp.auth";
+    private static boolean VALUE_PROPS = true;
+    // 发件人用户名、密码
+    private String SEND_USER = "2569000943@qq.com";
+    private String SEND_UNAME = "2569000943";
+    private String SEND_PWD = "********";
+    // 建立会话
+    private MimeMessage message;
+    private Session s;
+
+    /*
+     * 初始化方法
+     */
+    public SendMailUtil() {
+        Properties props = System.getProperties();
+        props.setProperty(KEY_SMTP, VALUE_SMTP);
+        props.put(KEY_PROPS, "true");
+        //props.put("mail.smtp.auth", "true");
+        s =  Session.getDefaultInstance(props, new Authenticator(){
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication(SEND_UNAME, SEND_PWD);
+            }});
+        s.setDebug(true);
+        message = new MimeMessage(s);
+    }
+
+    /**
+     * 发送邮件
+     *
+     * @param headName
+     *            邮件头文件名
+     * @param sendHtml
+     *            邮件内容
+     * @param receiveUser
+     *            收件人地址
+     */
+    public void doSendHtmlEmail(String headName, String sendHtml,
+                                String receiveUser) {
+        try {
+            // 发件人
+            InternetAddress from = new InternetAddress(SEND_USER);
+            message.setFrom(from);
+            // 收件人
+            InternetAddress to = new InternetAddress(receiveUser);
+            message.setRecipient(Message.RecipientType.TO, to);
+            // 邮件标题
+            message.setSubject(headName);
+            String content = sendHtml.toString();
+            // 邮件内容,也可以使纯文本"text/plain"
+            message.setContent(content, "text/html;charset=GBK");
+            message.saveChanges();
+            Transport transport = s.getTransport("smtp");
+            // smtp验证，就是你用来发邮件的邮箱用户名密码
+            transport.connect(VALUE_SMTP, SEND_UNAME, SEND_PWD);
+            // 发送
+            transport.sendMessage(message, message.getAllRecipients());
+            transport.close();
+            System.out.println("send success!");
+        } catch (AddressException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        SendMailUtil se = new SendMailUtil();
+        se.doSendHtmlEmail("邮件头文件名", "邮件内容", "798210413@qq.com");
+    }
+
+}
---
+++ sitetool/src/main/java/com/huawei/siteapp/mail/SendEmail.java
@@ -0,0 +1,76 @@
+package com.huawei.siteapp.mail;
+
+
+import java.security.Security;
+import java.util.Date;
+import java.util.Properties;
+
+import javax.mail.Authenticator;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.PasswordAuthentication;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+/**
+ * Created by z00390414 on 2017/6/16.
+ *
+ * @version 1.0
+ * @date 2017/6/16
+ */
+public class SendEmail {
+
+    public static void main(String[] args) throws AddressException,
+            MessagingException {
+        String SEND_USER = "694998138@qq.com";
+        String SEND_UNAME = "694998138";
+        String SEND_PWD = "Ishot@12";
+        String VALUE_SMTP = "smtp.exmail.qq.com";
+        Security.addProvider(new com.sun.net.ssl.internal.ssl.Provider());
+        //final String SSL_FACTORY = "javax.net.ssl.SSLSocketFactory";
+        // Get a Properties object
+        Properties props = System.getProperties();
+        // props.setProperty("mail.smtp.host", "smtp.gmail.com");
+        props.setProperty("mail.smtp.host", "smtp.exmail.qq.com");
+        //props.setProperty("mail.smtp.socketFactory.class", SSL_FACTORY);
+        //props.setProperty("mail.smtp.socketFactory.fallback", "false");
+        props.setProperty("mail.smtp.port", "25");
+        //props.setProperty("mail.smtp.port", "587");
+        //props.setProperty("mail.smtp.socketFactory.port", "25");
+        //props.setProperty("mail.smtp.socketFactory.port", "587");
+        props.put("mail.smtp.auth", "true");
+        final String username = "694998138";
+        final String password = "Ishot@12";
+        Session session = Session.getDefaultInstance(props,
+                new Authenticator() {
+                    protected PasswordAuthentication getPasswordAuthentication() {
+                        return new PasswordAuthentication(username, password);
+                    }
+                });
+
+        // -- Create a new message --
+        session.setDebug(true);
+        Message msg = new MimeMessage(session);
+
+        // -- Set the FROM and TO fields --
+        msg.setFrom(new InternetAddress(username + "@qq.com"));
+        msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(
+                "694998138@qq.com", false));
+        msg.setSubject("Hello---");
+        msg.setText("How are you");
+        msg.setSentDate(new Date());
+        Transport transport = session.getTransport("smtp");
+        // smtp验证，就是你用来发邮件的邮箱用户名密码
+        transport.connect(VALUE_SMTP, SEND_UNAME, SEND_PWD);
+        // 发送
+        transport.sendMessage(msg, msg.getAllRecipients());
+        Transport.send(msg);
+        transport.close();
+
+        System.out.println("Message sent.");
+    }
+
+}
