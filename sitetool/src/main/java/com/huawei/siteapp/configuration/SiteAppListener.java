01f1455b5589c646f66218467606eb0c8a988b60
zhaoyu <zhaoyuwy@163.com>
2017-07-07 01:14:12 +0800

    sql引入OK

--- sitetool/src/main/java/com/huawei/siteapp/configuration/SiteAppListener.java
+++ sitetool/src/main/java/com/huawei/siteapp/configuration/SiteAppListener.java
@@ -1,7 +1,7 @@
 package com.huawei.siteapp.configuration;
 
 import com.huawei.siteapp.model.UserModel;
-import com.huawei.siteapp.service.ExcelService.VmReportServiceImpl;
+import com.huawei.siteapp.repository.Impl.RepositoryTemplate;
 import com.huawei.siteapp.service.ModelService.IUserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,6 +10,7 @@ import org.springframework.web.context.WebApplicationContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+import java.sql.SQLException;
 
 /**
  * Created by z00390414 on 2017/6/28.
@@ -29,20 +30,14 @@ public class SiteAppListener implements ServletContextListener {
         webApplicationContext = (WebApplicationContext) servletContextEvent.getServletContext().getAttribute(WebApplicationContext.ROOT_WEB_APPLICATION_CONTEXT_ATTRIBUTE);
 
 
-        VmReportServiceImpl vmReportService = webApplicationContext.getBean(VmReportServiceImpl.class);
-
-//        IUserService userService = webApplicationContext.getBean("IUserService",IUserService.class);
-//        UserModel userDao=this.getObjectFromApplication(servletContextEvent.getServletContext(), UserModel.class);
-//        System.out.println("取得的Dao的实例="+userDao);
-
         System.out.println("ServletContex初始化");
         System.out.println(servletContextEvent.getServletContext().getServerInfo());
-//        RepositoryTemplate repositoryTemplate = SpringUtil.getBean(RepositoryTemplate.class);
-//        try {
-//            repositoryTemplate.executeSqlFile();
-//        } catch (SQLException e) {
-//            logger.error("",e);
-//        }
+        RepositoryTemplate repositoryTemplate = webApplicationContext.getBean(RepositoryTemplate.class);
+        try {
+            repositoryTemplate.executeSqlFile();
+        } catch (SQLException e) {
+            logger.error("",e);
+        }
         saveDefaultSystemUser();
     }
 
@@ -55,9 +50,7 @@ public class SiteAppListener implements ServletContextListener {
     private void saveDefaultSystemUser() {
         String defaultUserName = "admin";
         String defaultPwd = "Huawei123";
-//        IUserService userService = SpringUtil.getBean(IUserService.class);
         IUserService userService = webApplicationContext.getBean("IUserService", IUserService.class);
-//        UserModel userModel = userService.findOne((long)1);
 
         UserModel newUser = new UserModel();
         newUser.setUserType(1);
