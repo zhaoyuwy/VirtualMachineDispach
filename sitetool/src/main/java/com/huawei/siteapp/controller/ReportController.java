e79866d49bf6215b8fd410f3904bdc0cd49e8097
zhaoyu <zhaoyuwy@163.com>
2017-08-25 15:11:41 +0800

    测试OK localhost 更改为ip地址。 具体原因不清楚。url更改

--- sitetool/src/main/java/com/huawei/siteapp/controller/ReportController.java
+++ sitetool/src/main/java/com/huawei/siteapp/controller/ReportController.java
@@ -159,7 +159,7 @@ public class ReportController {
         Map<String, String> map = new HashMap<>();
         System.out.println(System.getProperty("user.dir"));
 
-        String reportName = "\\report\\2017-08-10 020004.xlsx";
+        String reportName = File.separator+"reportTest.xlsx";
         String path = System.getProperty("user.dir");
         map.put("reportPath", path+reportName);
         return map;
